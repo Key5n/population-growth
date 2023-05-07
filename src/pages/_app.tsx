ac4d3dee975c78961a5f29f379653a10ee8bd19e
Key5n <k.nogami.472@stn.nitech.ac.jp>
2023-05-08 03:23:41 +0900

    update: set pages config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { AppPropsWithLayout } from '@/lib/next/types';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+	const getLayout = Component.getLayout ?? ((page) => page);
+	return getLayout(<Component {...pageProps} />);
 }
