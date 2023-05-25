e237bc004ebe8bcd26e31ce7d995383d1898b39d
Key5n <k.nogami.472@stn.nitech.ac.jp>
2023-05-25 16:08:16 +0900

    fix: optimize the source code since it was full of waste

--- src/features/ui/Loading/Loading.component.tsx
+++ src/features/ui/Loading/Loading.component.tsx
@@ -5,24 +5,20 @@ import { useContextAndErrorIfNull } from '@/lib/context/useContextAndErrorIfNull
 
 export function Loading() {
 	const { isLoading } = useContextAndErrorIfNull(LoadingContext);
+
 	return (
 		// eslint-disable-next-line react/jsx-no-useless-fragment
 		<>
 			{isLoading && (
 				<div className={styles.overlay}>
 					<div className={styles.ldsSpinner}>
-						<div />
-						<div />
-						<div />
-						<div />
-						<div />
-						<div />
-						<div />
-						<div />
-						<div />
-						<div />
-						<div />
-						<div />
+						{
+							// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+							[...Array(12)].map((_, i) => (
+								// eslint-disable-next-line react/no-array-index-key
+								<div key={i} />
+							))
+						}
 					</div>
 				</div>
 			)}
